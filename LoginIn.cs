857b21997faab14a11b87c0d0d805f7ead2b971a
Lee-SDU <lidong8822668@gmailcom>
2014-11-19 09:32:15 +0800

    changealittle
    lakdsj

---
+++ LoginIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+
+namespace PDATestCSharp
+{
+    class LoginIn
+    {
+        /// <summary>
+        /// 检测输入的密码正确与否
+        /// </summary>
+        /// <returns>检测结果</returns>
+
+        public string login(string password)
+        {
+            string passwordreal = string.Empty;
+            passwordreal = IniOperator.GetINI("admin", "password", "", "\\Flash Disk\\Scanner\\config.ini");
+            if (password.Equals(""))
+            {
+                MessageBox.Show("请输入密码！");
+                return Message.NoUserOrPassword;
+            }
+            
+            if (passwordreal==password)
+
+            {
+                
+                return Message.LoginSuccess;
+            }
+            else
+            {
+                MessageBox.Show("密码错误，请重新输入");
+                
+                return Message.Wrong;
+            }
+            
+        }
+    }
+    class Message
+    {
+        public static string NoUserOrPassword = "用户名或者密码不能为空";
+        public static string LoginSuccess = "登录成功！";
+        public static string Wrong = "用户名或者密码不符，登录失败";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PDATestCSharp
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+            if (validateResult())
+            {
+            Application.Run(new Form1());asdfasdf
+            }
+        }
+        /// <summary>
+        /// 判断登录的密码正确与否
+        /// </summary>
+        /// <returns></returns>
+        static bool validateResult()
+        {
+            bool result = false;
+            Login form = new Login();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                result = true;
+
+
+            }
+            form.Dispose();
+            GC.Collect();
+            return result;
+
+        }
+    }
+}sdfs
